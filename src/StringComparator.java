81cbd02386fe3d2bc50926c83ac463493e2f8ce5
AlanHo <d0425434@fcu.edu.tw>
2016-10-28 02:31:45 +0800

    10/28更新

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class StringComparator {
 
 	public static void main(String[] args) {
-		String str1,str2;
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("please enter a string 1");
-		str1=scanner.next();
-		System.out.println("please enter a string 2");
-		str2=scanner.next();
-		if(str1.equalsIgnoreCase(str2) == true ){
-			System.out.println("The two strings are the same.");
-		}
-		else System.out.println("The two strings are not the same.");
-	}
+		int x = 1;
+		while (x == 1) {
+			String str1, str2;
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("please enter a string 1");
+			str1 = scanner.next();
+			System.out.println("please enter a string 2");
+			str2 = scanner.next();
+			if (str1.equalsIgnoreCase(str2) == true) {
+				System.out.println("The two strings are the same.");
+				x = 0;
+			} else {
+				System.out.println("The two strings are not the same.");
+				x = 1;
+			}
 
+		}
+	}
 }
